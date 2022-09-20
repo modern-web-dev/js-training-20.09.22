25f0a537212f956637ca54cc3bcdb226837fff9d
Marek Matczak <marek.matczak79@gmail.com>
2022-09-20 10:19:54 +0200

    objects

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-console.log('Hello world!');
+class Person {
+    constructor(firstName, lastName, age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+}
+
+const marek = new Person('Marek', 'Matczak', 43);
+const john = new Person('John', 'Example', 30);
+let person = {
+    '$first-name': 'John',
+    lastName: 'Example',
+    age: 30,
+    dateOfBirth: new Date(),
+    retired: true,
+    address: {
+        zipCode: 0
+    }
+};
+
+// person.address = undefined;
+// delete person.address;
+
+
+// console.log(Object.keys(person));
+// falsy values in JS: false, '', NaN, 0, null, undefined
+// truthy values in JS: [], {}
+
+// plain old JS -> ECMA2015 (ES6) -> ECMA2016...ECMA2022 -> ES.NEXT
+const zipCode = person?.address?.zipCode ?? 'zipCode not defined';
+console.log(zipCode);
+// if (person && person.address && person.address.zipCode) {
+//     console.log(person.address.zipCode);
+// }
+
